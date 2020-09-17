cf59944dab43323e6d5b15ef896ee6d10c4420e9
suyouwanggang <wanggang@etppm.com>
2020-09-17 11:47:05 +0800

    eventBus ,eventHelper

--- elements/utils/eventHelper.ts
+++ elements/utils/eventHelper.ts
@@ -1,6 +1,13 @@
 type ListenerResult= {
     destory: () => void;
 }
+/**
+ * @returns 返回一个对象，可以删除监听
+ * @param el DOM对象
+ * @param event 事件类型
+ * @param listener 监听
+ * @param options 事件监听options
+ */
 const addEvent = (el: Element|SVGAElement, event: string, listener: EventListener, options?: boolean | AddEventListenerOptions): ListenerResult => {
     el.addEventListener(event, listener, options);
     return {
@@ -8,5 +15,5 @@ const addEvent = (el: Element|SVGAElement, event: string, listener: EventListene
             el.removeEventListener(event, listener, options);
         }
     }
-}
+};
 export default addEvent;
