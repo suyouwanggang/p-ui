47f4c88dc985d1300a0bd6ae102fc06fcc9ff0e4
suyouwanggang <wanggang@etppm.com>
2020-10-10 18:45:54 +0800

    p-tree 过滤

--- components/tree/treeFillter.ts
+++ components/tree/treeFillter.ts
@@ -1,86 +1,78 @@
 import { ServerHttp2Session } from "http2";
 
- type TreeNodeData= {
+type TreeNodeData = {
     name?: string;/*节点名称*/
-    key?: string | number ;/* ID  */
+    key?: string | number;/* ID  */
     close?: boolean;/* 是否关闭 */
     seqNo?: number;/*同层序号*/
     icon?: string;/*节点图标 */
     child?: TreeNodeData[];/*下级节点 */
     closeable?: boolean;/*false,表示节点不能折叠起来 */
-    
+    _children?:TreeNodeData[];//内部过滤使用
 }
 
 
 /* 节点过滤器*/
- interface TreeFilter {
+interface TreeFilter {
     (data: TreeNodeData, ...arg: unknown[]): boolean;
 }
-const defaultFilter: TreeFilter =  (data: TreeNodeData, name:string, ...args:unknown[])=> {
-   
-    if (name == null || name === undefined || name === ''||name.trim()==='' ) {
+const defaultFilter: TreeFilter = (data: TreeNodeData, name: string, ...args: unknown[]) => {
+
+    if (name == null || name === undefined || name === '' || name.trim() === '') {
         return true;
     }
-    if (data&&data.name!=undefined) {
+    if (data && data.name !== undefined) {
         name = name.toLowerCase().trim();
         return data.name.toLowerCase().indexOf(name) !== -1;
     }
     return false;
 };
 
- const filterTreeDataArray = (root:TreeNodeData,filter: TreeFilter,...filterObject:unknown[])=> {
-        const map=new Map<TreeNodeData,Array<TreeNodeData>>();
-        const map2=new Map<TreeNodeData,Set<TreeNodeData>>();
-        //key 为node, value:为下级满足的节点
-
-        const parentMap=new Map<TreeNodeData,TreeNodeData>();
-        parentMap.set(root,undefined );
-        //key 为node, value:为父节点
-        const iteratorFun=(node:TreeNodeData,parent:TreeNodeData)=>{
-            parentMap.set(node,parent);
-            // console.log(node.name);
-            if(node!=root &&filter.apply(null,[node, ...filterObject])){
-                // console.log('match=='+node.name);
-                let tempNode=node;
-                let tempParentNode=parentMap.get(tempNode);
-                labelWle: while(tempParentNode){
+const filterTreeDataArray = (root: TreeNodeData, filter: TreeFilter, ...filterObject: unknown[]):TreeNodeData => {
+    const map = new Map<TreeNodeData, Array<TreeNodeData>>();
+    const setAddNode = new Set<TreeNodeData>();
+    //key 为node, value:为下级满足的节点
+    const parentMap = new Map<TreeNodeData, TreeNodeData>();
+    parentMap.set(root, undefined);
+    //key 为node, value:为父节点
+    const iteratorFun = (node: TreeNodeData, parent: TreeNodeData) => {
+        parentMap.set(node, parent);
+        // console.log(node.name);
+        if (node !== root && filter.apply(null, [node, ...filterObject])) {
+            // console.log('match=='+node.name);
+            let tempNode = node;
+            let tempParentNode = parentMap.get(tempNode);
+            if(!setAddNode.has(tempParentNode)){
+                setAddNode.add(tempParentNode  );
+                while (tempParentNode) {
                     //console.log('tempname=='+ temp.name);
-                    let set=map2.get(tempParentNode);
-                    if(set==undefined){
-                        set=new Set( );
-                        map2.set(tempParentNode,set);
+                    let matchSub = map.get(tempParentNode);
+                    if (matchSub === undefined) {
+                        matchSub = [];
+                        map.set(tempParentNode, matchSub);
                     }
-                    if(!set.has(tempNode)){
-                        set.add(tempNode);
-                        let matchSub=map.get(tempParentNode);
-                        if(matchSub==undefined){
-                            matchSub=[];
-                            map.set(tempParentNode,matchSub);
-                        }
-                        matchSub.push(tempNode);
-                        tempNode=tempParentNode;
-                        tempParentNode=parentMap.get(tempParentNode);
-                    }else{
-                        break labelWle;
-                    }
+                    matchSub.push(tempNode);
+                    tempNode = tempParentNode;
+                    tempParentNode = parentMap.get(tempParentNode);
                 }
             }
-            if(node.child){
-                node.child.forEach((item) =>iteratorFun(item,node));
-            }
         }
-       iteratorFun(root,null);
-       const iteratorMap=(node:TreeNodeData) =>{
-         const sub=map.get(node);
-         if(sub!=undefined){
-             node.child=sub;
-             sub.forEach(item => iteratorMap(item));
-         }else{
-             node.child=[];
-         }
-       }
-       iteratorMap(root);
-       return root;
+        if (node.child) {
+            node.child.forEach((item) => iteratorFun(item, node));
+        }
+    }
+    iteratorFun(root, null);
+    const iteratorMap = (node: TreeNodeData) => {
+        const sub = map.get(node);
+        if (sub !== undefined) {
+            node._children = sub;
+            sub.forEach( (item) => iteratorMap(item));
+        } else {
+            node._children = [];
+        }
+    }
+    iteratorMap(root);
+    return root;
 };
 
 
@@ -91,27 +83,27 @@ const defaultFilter: TreeFilter =  (data: TreeNodeData, name:string, ...args:unk
  * @param filter 
  * @param args 
  */
-  const filterTreeData = (root: TreeNodeData, filter: TreeFilter, ...args: unknown[]): TreeNodeData => {
+const filterTreeData = (root: TreeNodeData, filter: TreeFilter, ...args: unknown[]): TreeNodeData => {
     const cloneRoot: TreeNodeData = JSON.parse(JSON.stringify(root));
     if (filter == null) {
         return cloneRoot;
     }
-    const iteratorNode=(node:TreeNodeData,node2:TreeNodeData) =>{
-        Object.defineProperty(node,'_source',{
-            configurable:false,
-            writable:false,
-            enumerable:false,
-            value:node2
+    const iteratorNode = (node: TreeNodeData, node2: TreeNodeData) => {
+        Object.defineProperty(node, '_source', {
+            configurable: false,
+            writable: false,
+            enumerable: false,
+            value: node2
         });
-        if(node.child){
-            node.child.forEach((item,index)=>{
-                iteratorNode(item,node2.child[index]);
+        if (node.child) {
+            node.child.forEach((item, index) => {
+                iteratorNode(item, node2.child[index]);
             })
         }
     }
-    iteratorNode(cloneRoot,root);
-    const argArray=[cloneRoot,filter,...args];
-    return filterTreeDataArray.apply(null,argArray);
+    iteratorNode(cloneRoot, root);
+    const argArray = [cloneRoot, filter, ...args];
+    return filterTreeDataArray.apply(null, argArray);
 }
 
 
@@ -121,8 +113,8 @@ const defaultFilter: TreeFilter =  (data: TreeNodeData, name:string, ...args:unk
  * @param root 根节点
  * @returns 返回根节点
  */
-  const listTreeDataToRoot = (nodeList: TreeNodeData[], root: TreeNodeData = {}): TreeNodeData => {
-    const map = new Map<string | number , Array<TreeNodeData>>();
+const listTreeDataToRoot = (nodeList: TreeNodeData[], root: TreeNodeData = {}): TreeNodeData => {
+    const map = new Map<string | number, Array<TreeNodeData>>();
     nodeList.forEach((item: TreeNodeData) => {
         let sub = map.get(item.key);
         if (sub === undefined) {
@@ -133,7 +125,7 @@ const defaultFilter: TreeFilter =  (data: TreeNodeData, name:string, ...args:unk
     });
     const iteratorFun = (key: string | number, data: TreeNodeData) => {
         const subChild = map.get(key);
-        if(subChild){
+        if (subChild) {
             subChild.forEach((item: TreeNodeData) => {
                 if (!data.child) {
                     data.child = [];
@@ -142,7 +134,7 @@ const defaultFilter: TreeFilter =  (data: TreeNodeData, name:string, ...args:unk
                 iteratorFun(item.key, item);
             });
         }
-        
+
     };
     iteratorFun(root.key, root);
     return root;
@@ -168,4 +160,4 @@ const findDataByKey = function (data: TreeNodeData, key: string | number): TreeN
 };
 
 
-export {TreeNodeData,TreeFilter, listTreeDataToRoot,filterTreeData,defaultFilter,findDataByKey};
+export { TreeNodeData, TreeFilter, listTreeDataToRoot, filterTreeData, defaultFilter, findDataByKey };
