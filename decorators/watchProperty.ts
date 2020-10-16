7316c05e3fc7d088c0a9e4556e43505ae6934667
suyouwanggang <wanggang@etppm.com>
2020-10-16 14:18:12 +0800

    @watch, @watchProperty

--- decorators/watchProperty.ts
+++ decorators/watchProperty.ts
@@ -1,3 +1,5 @@
+import { PropertyDeclaration } from "lit-element";
+
 /**
  * 属性监听回调方法。
  * @param value: 属性新的值
@@ -8,45 +10,46 @@
 export type CallBackFunction<T> = (value: T, oldValue: T, name: PropertyKey, isFirstChange: boolean) => void;
 
 /**
- * 属性改变监听器 ,如果跟 @property 一起使用，则必须放在property 的前面 ,否则@property 不起作用
  *
+ * @watch, @property 合并。
+ * 将属性设置为property ，同时能通过@watchMethodName 监听属性变化
  * 监听方法 ，接收参数四个参数：
  *   value:当前值，oldValue:历史值，
  *   name:属性名称，
  *   isFirst:是否是第一次设置
- * @param watchMethodName 监听的方法的名称，或者 直接是监听方法
+ * @param watchMethodName 监听的方法的名称，或者直接是监听方法（方法调用的this 为当前组件)
+ * @param options 同@property 参数一致
  */
-export default function watch<T>(watchMethodName: string | CallBackFunction<T>) {
-    const cachedValueKey = Symbol();
-    const isFirstChangeKey = Symbol();
+export default function watchProperty<T>(watchMethodName: string | CallBackFunction<T>, options?: PropertyDeclaration) {
     // tslint:disable-next-line: no-any
     return (protoOrDescriptor: any, name: PropertyKey) => {
         const callBackFn: CallBackFunction<T> = typeof watchMethodName === 'string' ? protoOrDescriptor[watchMethodName] : watchMethodName;
         if (!callBackFn) {
-            console.warn(`Cannot find method ${watchMethodName} in class ${protoOrDescriptor.constructor.name} or watchMethodName is not a function `);
+            console.warn(`Cannot find method ${watchMethodName} in class ${protoOrDescriptor.constructor.name} or watchMethodName is not a function`);
         }
-        const oldProperty = Object.getOwnPropertyDescriptor(protoOrDescriptor, name);
+        const isFirstChangeKey = Symbol();
+        const key =  Symbol() ;
         Object.defineProperty(protoOrDescriptor, name, {
             configurable: true,
             enumerable: true,
             set(value) {
+                const oldValue = this[key];
                 const isFirst = this[isFirstChangeKey] === undefined;
-                const oldValue = this[cachedValueKey];
                 if (!isFirst && oldValue === value) {
                     return;
                 }
-                this[cachedValueKey] = value;
-                this[isFirstChangeKey] = false;
+                this[key] = value;
                 if (callBackFn) {
                     callBackFn.call(this, value, oldValue, name, isFirst);
                 }
-                if (oldProperty != null && oldProperty.set) {
-                    oldProperty.set.call(this, value);
+                this[isFirstChangeKey] = false;
+                if (this && this.requestUpdateInternal) {
+                    this.requestUpdateInternal(name, oldValue, options);
                 }
             },
             // tslint:disable-next-line: no-any
             get(this: any) {
-                return oldProperty && oldProperty.get ? oldProperty.get.call(this) : this[cachedValueKey];
+                return this[key];
             }
         });
     };
--- testModel/test-one.ts
+++ testModel/test-one.ts
@@ -1,5 +1,7 @@
 import { LitElement, html, css, customElement, property } from 'lit-element';
 import model from '../directives/model';
+import watch from '../decorators/watch';
+import watchProperty from '../decorators/watchProperty';
 type Person = {
     userName: string;
     displayName: string;
@@ -14,6 +16,12 @@ type Person2 = {
 };
 @customElement('p-test-one')
 export default class TestOne extends LitElement {
+    @watchProperty('changeTestManage')
+    testName:string='test';
+    changeTestManage(value:string, oldValue:string,name:PropertyKey,isFirstChange:boolean){
+        console.log(`value=${value}  oldvalue=${oldValue} name=${String(name)} isFirstChange=${isFirstChange}`);
+    }
+
     @property({ attribute: false })
     person: Person = {
         userName: 'wanggang',
@@ -24,6 +32,7 @@ export default class TestOne extends LitElement {
     array: Person2[]=[];
     constructor(){
         super();
+       
         for (let i = 0; i < 10; i++) {
             let p = {
                 user:{
@@ -35,6 +44,7 @@ export default class TestOne extends LitElement {
             this.array.push(p);
         }
     }
+    
     renderArray(){
     return this.array.map( (tempPerson: Person2) =>
             html`
@@ -52,9 +62,23 @@ export default class TestOne extends LitElement {
     }
     firstUpdated( _changedProperties: Map<string | number | symbol, unknown> ){
         super.firstUpdated(_changedProperties);
+        this.testName='test.73054465e6890de20c7e.hot-update.js.map';
+    }
+    update(changedProperties: Map<string | number | symbol, unknown>){
+        console.log(' ----update ---');
+        super.update(changedProperties);
+    }
+
+    updated(changedProperties: Map<string | number | symbol, unknown>){
+        console.log(' ----updatedupdatedupdated ---');
+        super.update(changedProperties);
+    }
+    renderTestName(){
+        return html`<div><input type='text' .value=${this.testName} @change=${(event:Event) =>{this.testName=(event.target as any).value;}} /></div>`;
     }
     render() {
         return html`<div>
+            ${this.renderTestName()}
              <span>${model(this, 'person.color')}</span>
                  <p-color .value=${model(this, 'person.color', 'change')}></p-color>
                 <span>${model(this.person, 'userName')}</span>
