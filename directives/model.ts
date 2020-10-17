18c66468392f209ecc419abb67551c6173c407da
suyouwanggang <wanggang@etppm.com>
2020-10-17 14:31:38 +0800

    model 组件增强。 循环组件

--- directives/model.ts
+++ directives/model.ts
@@ -1,24 +1,28 @@
 import { directive, Part, AttributePart, PropertyPart, BooleanAttributePart } from 'lit-html';
-type CacheNode = {
-    [key: string]: Set<Part>;
-};
+// tslint:disable-next-line: no-any
+type CacheNode = Map<string | ((modelValue: any) => any) | undefined, Set<Part>>;//缓存 被监听对象 ，有哪些node 需要被修改的
 const objectDependsCache = new WeakMap<Object, CacheNode>();
-const addDependsToCache = (modeValue: Object, fieldPath: string, part: Part) => {
+const addDependsToCache = (modeValue: Object, fieldPath: string | ((modelValue: any) => any) | undefined, part: Part) => {
     let node = objectDependsCache.get(modeValue);
     if (node === undefined) {
-        node = {};
+        // tslint:disable-next-line: no-any
+        node = new Map<string | ((modelValue: any) => any) | undefined, Set<Part>>();
         objectDependsCache.set(modeValue, node);
     }
-    let set = node[fieldPath];
+    let set = node.get(fieldPath);
     if (set === undefined) {
         set = new Set<Part>();
-        node[fieldPath] = set;
+        node.set(fieldPath, set);
     }
     set.add(part);
 };
 // tslint:disable-next-line: no-any
-const getDataFieldValue = (data: any, field: string ) => {
-    if (data && field) {
+const getDataFieldValue = (data: any, field: string | ((modelValue: any) => any) | undefined) => {
+    if (field === undefined || field == null) {
+        return data;
+    } else if (typeof field === 'function') {
+        return field(data);
+    } else if (typeof field === 'string') {
         if (field.indexOf('.') === -1) {
             return data[field];
         }
@@ -48,9 +52,8 @@ const setDataFieldValue = (data: any, field: string, value: any) => {
     }
     const cache = objectDependsCache.get(data);
     if (cache !== undefined) {
-        for (const key in cache) {
+        for (const [key, set] of cache) {
             const tempValue = getDataFieldValue(data, key);
-            const set = cache[key];
             for (const d of set) {
                 if (d.value !== tempValue) {
                     d.setValue(tempValue);
@@ -68,10 +71,10 @@ const oldEventTypeSymbol = Symbol();
 /**
  * 实现类似v-model 功能。
  * @param modelObject 需要被跟踪的对象object
- * @param fieldValue 对象属性，支持按照“.” 分隔。
- * @param eventName 如果绑定Atrtribute，或者property ,则通过 eventName 更新model 值，默认为input 事件。
+ * @param fieldValue 对象属性，支持按照“.” 分隔。,或者是一个函数，将对象转换需要的值
+ * @param eventName 如果绑定Atrtribute，或者property ,则通过 eventName 更新model 值，默认为input 事件，此时fieldValue 必须是modeleObject 的属性值。
  */
-const model = directive((modelObject: Object, fieldValue: string, eventName: string = 'input') => (part: Part) => {
+const model = directive((modelObject: Object, fieldValue: string |((modelValue: any) => any) | undefined, eventName: string = 'input') => (part: Part) => {
     addDependsToCache(modelObject, fieldValue, part);
     const result = getDataFieldValue(modelObject, fieldValue);
     let element: Element = null;
@@ -80,7 +83,7 @@ const model = directive((modelObject: Object, fieldValue: string, eventName: str
     } else if (part instanceof BooleanAttributePart) {
         element = part.element;
     }
-    if (element != null) {
+    if (element != null&&typeof fieldValue ==='string') {
         // tslint:disable-next-line: no-any
         let oldListener = (element as any)[oldListenerSymbol];
         // tslint:disable-next-line: no-any
@@ -99,9 +102,9 @@ const model = directive((modelObject: Object, fieldValue: string, eventName: str
         };
         element.addEventListener(eventName, oldListener);
         // tslint:disable-next-line: no-any
-        (element as any).oldListenerSymbol = oldListener;
+        (element as any)[oldListenerSymbol] = oldListener;
         // tslint:disable-next-line: no-any
-        (element as any).oldEventTypeSymbol = eventName;
+        (element as any)[oldEventTypeSymbol] = eventName;
     }
     const preivious = previousValueMap.get(part);
     if (preivious === undefined || preivious !== result) {
--- testModel/test-one.ts
+++ testModel/test-one.ts
@@ -1,6 +1,6 @@
 import { LitElement, html, css, customElement, property } from 'lit-element';
 import model from '../directives/model';
-import watch from '../decorators/watch';
+import '../components/iterator';
 import watchProperty from '../decorators/watchProperty';
 type Person = {
     userName: string;
@@ -8,19 +8,21 @@ type Person = {
     color: string;
 };
 type Person2 = {
-    user:{
-        userName:string,
-        displayName:string,
+    user: {
+        userName: string,
+        displayName: string,
     };
     color: string;
 };
 @customElement('p-test-one')
 export default class TestOne extends LitElement {
     @watchProperty('changeTestManage')
-    testName:string='test';
-    changeTestManage(value:string, oldValue:string,name:PropertyKey,isFirstChange:boolean){
+    testName: string = 'test';
+    changeTestManage(value: string, oldValue: string, name: PropertyKey, isFirstChange: boolean) {
         console.log(`value=${value}  oldvalue=${oldValue} name=${String(name)} isFirstChange=${isFirstChange}`);
     }
+    @property({ attribute: false })
+    userMap = new Map<number, Person2>();
 
     @property({ attribute: false })
     person: Person = {
@@ -29,62 +31,74 @@ export default class TestOne extends LitElement {
         color: 'red'
     };
     @property({ attribute: false })
-    array: Person2[]=[];
-    constructor(){
+    array: Person2[] = [];
+    constructor() {
         super();
-       
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 10000; i++) {
             let p = {
-                user:{
+                user: {
                     userName: 'wanggang' + i,
                     displayName: '王刚' + i,
                 },
                 color: 'red'
             };
+            this.userMap.set(i, p);
             this.array.push(p);
         }
     }
-    
-    renderArray(){
-    return this.array.map( (tempPerson: Person2) =>
+    renderArray() {
+        return this.array.map((tempPerson: Person2) =>
             html`
                 <div>
-                    <span style='background-color:${model(tempPerson, 'color')};width:100px;display:inline-block'>${model(tempPerson, 'color')} ${model(tempPerson, 'color')}</span>
-                    <p-color .value=${model(tempPerson, 'color', 'change')}></p-color>
+                    <span style='background-color:${model(tempPerson,'color')};width:300px;display:inline-block'>${model(tempPerson,
+                        'color')} </span>
+                    <p-color .value=${model(tempPerson, 'color' , 'change' )}></p-color>
                     <span>${model(tempPerson, 'user.userName')}</span>
                     <span>${model(tempPerson, 'user.displayName')}</span>
-                    <p-input type='text' placeholder='userName' .value=${model(tempPerson, 'user.userName')} ></p-input>
-                    <input type='text' placeholder='userName' .value=${model(tempPerson, 'user.userName')} />
-                    <input type='text' name='displayname' placeholder='displayName' .value=${model(tempPerson, 'user.displayName')} />
+                    <p-input type='text' placeholder='userName' .value=${model(tempPerson, 'user.userName' )}></p-input>
+                    <input type='text' placeholder='userName' .value=${model(tempPerson, 'user.userName' )} />
+                    <input type='text' name='displayname' placeholder='displayName' .value=${model(tempPerson, 'user.displayName' )} />
                 </div>
             `
         );
     }
-    firstUpdated( _changedProperties: Map<string | number | symbol, unknown> ){
-        super.firstUpdated(_changedProperties);
-        this.testName='test.73054465e6890de20c7e.hot-update.js.map';
+    static templateFun = (value: Person, index: number) => {
+        return html`<div> array: ${index + 1}. ${model(value, (valueObj) => JSON.stringify(valueObj))}</div>`;
     }
-    update(changedProperties: Map<string | number | symbol, unknown>){
+    static templateMap = (value: Person, index: number) => {
+        return html`<div> key=${index}. value=${model(value, (valueObj) => JSON.stringify(valueObj))}</div>`;
+    }
+    renderIterator() {
+        return html`<p-iterator .value=${this.array} .template=${TestOne.templateFun}></p-iterator>`;
+    }
+    iteratorMap() {
+        return html`<p-iterator .value=${this.userMap} .template=${TestOne.templateMap}></p-iterator>`;
+    }
+    firstUpdated(_changedProperties: Map<string | number | symbol, unknown>) {
+        super.firstUpdated(_changedProperties);
+    }
+    update(changedProperties: Map<string | number | symbol, unknown>) {
         console.log(' ----update ---');
         super.update(changedProperties);
     }
 
-    updated(changedProperties: Map<string | number | symbol, unknown>){
+    updated(changedProperties: Map<string | number | symbol, unknown>) {
         console.log(' ----updatedupdatedupdated ---');
         super.update(changedProperties);
     }
-    renderTestName(){
-        return html`<div><input type='text' .value=${this.testName} @change=${(event:Event) =>{this.testName=(event.target as any).value;}} /></div>`;
+    renderTestName() {
+        return html`<div><input type='text' .value=${this.testName} @change=${(event: Event)=> { this.testName = (event.target as any).value; }}
+    /></div>`;
     }
     render() {
         return html`<div>
-            ${this.renderTestName()}
-             <span>${model(this, 'person.color')}</span>
-                 <p-color .value=${model(this, 'person.color', 'change')}></p-color>
-                <span>${model(this.person, 'userName')}</span>
-                <input type='text' placeholder='userName' .value=${model(this.person, 'userName')} />
-                <span>${model(this.person, 'displayName')}</span>
-                <input type='text' name='displayname' placeholder='displayName' .value=${model(this.person, 'displayName')} />
-        </div>${this.renderArray()}` ;
+    ${this.renderTestName()}
+    <span>${model(this, 'person.color')}</span>
+    <p-color .value=${model(this, 'person.color' , 'change' )}></p-color>
+    <span>${model(this.person, (a: Person) => JSON.stringify(a))}</span>
+    <input type='text' placeholder='userName' .value=${model(this.person, 'userName' )} />
+    <span>${model(this.person, 'displayName')}</span>
+    <input type='text' name='displayname' placeholder='displayName' .value=${model(this.person, 'displayName' )} />
+</div> ${this.renderIterator()}`;
     }
 }
